a1c17957538bb36ea407ba582f93f4bcefb40460
Corrie <corriejohnstone98@hotmail.com>
2024-04-25 15:49:34 +0100

    creation of Beastmaster finalized
    Building blocks imported for the Beastmaster character, as well as the beginnings of the necessary draw calls to SHOW these assets on screen.

--- appearance.gd
+++ appearance.gd
@@ -10,77 +10,33 @@ class_name Appearance extends Resource
 @export var portrait : CompressedTexture2D
 @export var portrait_frame_data : Vector2i
 
-# TODO need more armour types - currently this only covers a
-# single armour segment (ie Beastmaster_Back) when there are several
-# that need a category. Array[Array[CompressedTexture2D]] is not
-# supported so i need another method. Maybe an exported int to
-# instantiate new arrays as per how many clothing slots are needed.
-
-@export_category("Clothing slots")
-@export var clothing_slot : Array[ClothingSlot]
-
-@export_category("OBSOLETE!!!! Armour and clothing spritesheets") 
-@export var armour_slots : Array[String]
-@export var armour_spritesheets : Array[CompressedTexture2D]
-@export var armour_spritesheets_frame_data : Array[Vector2i]
-
-@export_category("Weapon spritesheets")
-@export var weapon_spritesheets : Array[CompressedTexture2D]
-@export var weapon_spritesheets_frame_data : Array[Vector2i]
+@export_category("Gear slots")
+@export var clothing_slots : Array[ClothingSlot]
 
 func _ready():
-	populate_arh(null,body_spritesheets,
-	body_spritesheets_frame_data,
-	armour_spritesheets,
-	armour_spritesheets_frame_data,
-	weapon_spritesheets,weapon_spritesheets_frame_data)
+	populate_arh(null,body_spritesheets, body_spritesheets_frame_data, clothing_slots)
 
-
-# This is just silly, late night code. Theres a future issue here.
-func populate_arh(arh : AppearanceResourceHandler, base_spritesheet : Array[CompressedTexture2D], base_spritesheet_frame_data : Array[Vector2i],armour_spritesheets : Array[CompressedTexture2D], armour_spritesheet_frame_data : Array[Vector2i], weapon_spritesheets : Array[CompressedTexture2D], weapon_spritesheets_frame_data : Array[Vector2i]):
-	
-	# aliases to make this absolute mess easier to read
-	var bss = base_spritesheet
-	var bss_fd = base_spritesheet_frame_data
-	var ass = armour_spritesheets
-	var ass_fd = armour_spritesheets_frame_data
-	var wss = weapon_spritesheets
-	var wss_fd = weapon_spritesheets_frame_data
-	
+func populate_arh(arh : AppearanceResourceHandler, base_spritesheet : Array[CompressedTexture2D], base_spritesheet_frame_data : Array[Vector2i],gear_slots : Array[ClothingSlot]):
 	# Ensure there is a resource handler, otherwise create one
 	# zero every array, and populate it with what is passed in
 	if arh == null:
 		arh = AppearanceResourceHandler.new()
 	else:
 		arh.base_spritesheets.clear()
-		arh.base_spritesheets.append_array(bss)
+		arh.base_spritesheets.append_array(base_spritesheet)
 		
 		arh.base_spritesheets_frame_data.clear()
-		arh.base_spritesheets_frame_data.append_array(bss_fd)
+		arh.base_spritesheets_frame_data.append_array(base_spritesheet_frame_data)
 		
-		arh.clothing_spritesheets.clear()
-		arh.clothing_spritesheets.append_array(ass)
-		
-		arh.clothing_spritesheets_frame_data.clear()
-		arh.clothing_spritesheets_frame_data.append_array(ass_fd)
-		
-		arh.equippable_spritesheets.clear()
-		arh.equippable_spritesheets.append_array(wss)
-		
-		arh.equippable_spritesheets_frame_data.clear()
-		arh.equippable_spritesheets_frame_data.append_array(wss_fd)
+		arh.gear_slots.clear()
+		arh.gear_slots.append_array(gear_slots)
+
 
 class AppearanceResourceHandler extends Appearance:
 	
 	var base_spritesheets : Array[CompressedTexture2D]
 	var base_spritesheets_frame_data : Array[Vector2i]
-	var clothing_spritesheets : Array[CompressedTexture2D]
-	var clothing_spritesheets_frame_data : Array[Vector2i]
-	var equippable_spritesheets : Array[CompressedTexture2D]
-	var equippable_spritesheets_frame_data : Array[Vector2i]
-	
-	func _get_array_size(arr : Array) -> int:
-		return arr.size()
+	var gear_slots : Array[ClothingSlot]
 	
 	func vector_to_frame_data(v2in : Vector2i, vframe : int, hframe : int) -> void:
 		if v2in != null:
@@ -89,3 +45,12 @@ class AppearanceResourceHandler extends Appearance:
 		else:
 			vframe = 1
 			hframe = 1
+	
+	func unpack_gear_arrays():
+		pass
+	
+	func resource_to_sprite(cs : ClothingSlot):
+		pass
+	
+	func get_sprites():
+		pass
--- scene/global/game_character.gd
+++ scene/global/game_character.gd
@@ -2,5 +2,18 @@ class_name GameCharacter extends UnitConstructor
 
 @export var unit_scene_import : PackedScene
 
+@onready var t_appearance = unit_scene_import.apperance_resource
 @onready var t_portrait = $"----- Spritesheets -----/Portrait"
 @onready var t_spritesheets : Array[Sprite2D]
+
+@onready var _arh = Appearance.AppearanceResourceHandler.new()
+
+# TODO Dynamically create new Sprite2Ds for each spritesheet packed
+# within the imported GameCharacter PackedScene. 
+
+func _ready():
+	draw_portrait()
+
+func draw_portrait() -> void:
+	t_portrait.texture = t_appearance.portrait
+	_arh.vector_to_frame_data(t_appearance.portrait_frame_data, t_portrait.vframes,t_portrait.hframes)
