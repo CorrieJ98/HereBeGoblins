7cd88af5722caf6a60fa297b0e4a57b5234f4c75
Corrie <corriejohnstone98@hotmail.com>
2024-05-07 11:45:13 +0100

    new

--- scene/global/game_controller.gd
+++ scene/global/game_controller.gd
@@ -118,4 +118,52 @@ func _on_area_selected(box):
 		u.set_selected(!u.is_selected)
 
 func _on_unit_selected(box):
+<<<<<<< Updated upstream
 	pass # Replace with function body.
+=======
+	pass
+
+
+
+# TODO New function for command unit selection, taking a callable value of
+# each possible UnitCommand enum
+func command_move_unit_selection(selection : Array[Unit]) -> void:
+	var is_player : bool = false
+	var vel : Vector2
+	
+	for each in selection:
+		
+		# Firstly, ensure the selected unit does belong to the player
+		# otherwise, deselect all units
+		is_player = each.get_is_controllable()
+		if is_player == false:
+			selection.clear()
+			break
+		else:
+			move_cmd_(each)
+
+func move_cmd_(unit : Unit):
+	var vp_mouse = get_viewport().get_mouse_position()
+	var dt_movespeed = unit.profile.speed_walk * get_process_delta_time()
+	var dist_to_move_point = vp_mouse - unit.position
+	var dir : Vector2 
+	
+	if (unit.velocity.length() <= unit.profile.speed_walk):
+		dir = unit.velocity
+		dir *= dt_movespeed
+		unit.velocity += dir
+	
+	if dist_to_move_point.length() <= 100:
+		print("within 100, stopping")
+		unit.velocity.move_toward(unit.position,1.0)
+	
+	#unit.velocity.angle_to_point(vdir)
+	#unit.velocity.normalized()
+	#unit.velocity.move_toward(vdir,get_process_delta_time() * unit.profile.speed_walk) 
+	#unit.move_and_slide()
+	#print(unit.velocity, " vel")
+	#print(vdir, " vdir")
+
+func attack_cmd_(unit : Unit):
+	pass
+>>>>>>> Stashed changes
